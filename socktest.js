90621344ab6d70da17bff76689c05c0ab2fc06b8
Eugene Dobry <Gingy@HackBook-Pro.local>
2014-04-02 10:13:18 -0400

    testing data uri rendering

--- socktest.js
+++ socktest.js
@@ -1,10 +1,11 @@
 var io = require('socket.io').listen(80);
 var ss = require('socket.io-stream');
 var path = require('path');
+var fs = require("fs");
 
 io.of('/user').on('connection', function(socket) {
   ss(socket).on('profile-image', function(stream, data) {
-    var filename = path.basename(data.name);
-    stream.pipe(fs.createWriteStream(filename));
+    debugger;
+    stream.pipe(fs.createWriteStream("./" + data));
   });
 });
