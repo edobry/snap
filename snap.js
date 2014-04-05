d8c7d735611329beab1932f40cf4cbb60d8fec3a
Eugene Dobry <Gingy@hackbook-pro.home>
2014-04-05 13:41:10 -0400

    render streamed data from first snap to canvas

--- snap.js
+++ snap.js
@@ -2,43 +2,47 @@ var fs = require("fs"),
     sys = require("sys"),
     util = require("./util"),
     client = require("./client");
-
-var saveSnaps = function (snaps) {    
-    // Make sure the images folder exists
-    if (!fs.existsSync('./images'))
-        fs.mkdirSync('./images');
-    
-    snaps.forEach(function(snap) {
-        // Make sure the snap item is unopened and sent to you (not sent by you)
-        if (typeof snap.sn !== 'undefined' && typeof snap.t !== 'undefined' && snap.st == 1) {
-            console.log('Saving snap from ' + snap.sn + '...');
-
-            // Save the image to ./images/{SENDER USERNAME}_{SNAP ID}.jpg
-            var stream = fs.createWriteStream('./images/' + snap.sn + '_' + snap.id + '.jpg', {
-                flags: 'w',
-                encoding: null,
-                mode: 0666
-            });
-            client.getBlob(snap.id).then(function(blob) {
-                blob.pipe(stream);
-                blob.resume();
-            });
-        }
-    });
-};
-
-var showSnaps = function (snaps) {
-    snaps.forEach(function(snap) {
-        
-    });
-};
+var io = require('socket.io');
+var ss = require('socket.io-stream');
+var path = require('path');
+var fs = require("fs");
 
 var stdin = process.openStdin(),
     stdout = process.stdout;
 
+var _snaps = [];
+
+var listen = function () {
+    io = io.listen(80);
+    console.log("Listening on port 80");
+    io.of('/test').on('connection', function(socket) {
+        socket.on('ready', function (){
+            //_snaps.forEach(function(snap) {
+                // Make sure the snap item is unopened and sent to you (not sent by you)
+                var snap = _snaps[0];
+                if (typeof snap.sn !== 'undefined' && typeof snap.t !== 'undefined' && snap.st == 1) {
+                    console.log('Sending snap from ' + snap.sn + '...');
+
+                    client.getBlob(snap.id).then(function(blob) {
+                        var stream = ss.createStream();
+                        ss(socket).emit('blob', stream);
+
+                        blob.pipe(stream);
+                        blob.resume();
+                    });
+                }
+            //});
+        });
+    });
+};
+
 var ask = util.makeAsker(stdin, stdout);
 ask("Username", function(user) {
     ask("Password", function(pass) {
-        client.getSnaps(user, pass, saveSnaps);
+        client.getSnaps(user, pass, function (snaps) {
+            _snaps = snaps;
+
+            listen();
+        });
     });
 });
